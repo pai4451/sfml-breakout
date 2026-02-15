7d0b7e46e1d9da7265a3d1bd8fce8f8027c306a6
pai4451 <b04504095@ntu.edu.tw>
2026-02-15 15:44:42 +0800

    add brick

--- Project 01 Basic Window/constants.h
+++ Project 01 Basic Window/constants.h
@@ -12,6 +12,11 @@ struct constants {
 	static constexpr float paddle_width{ 60.0f };
 	static constexpr float paddle_height{ 20.0f };
 	static constexpr float paddle_speed{ 8.0f };
+	static constexpr float brick_width{ 43.0f };
+	static constexpr float brick_height{ 20.0f };
+	static constexpr float brick_offset{ brick_width / 2.0f };
+	static constexpr int brick_columns{ 10 };
+	static constexpr int brick_rows{ 4 };
 };
 
 #endif // CONSTANTS_H
--- Project 01 Basic Window/main.cpp
+++ Project 01 Basic Window/main.cpp
@@ -4,9 +4,11 @@
 #include <SFML/Graphics.hpp>
 
 #include <string>
+#include <vector>
 
 #include "background.h"
 #include "ball.h"
+#include "brick.h"
 #include "constants.h"
 #include "interactions.h"
 #include "paddle.h"
@@ -24,12 +26,27 @@ int main() {
 	// Create a paddle object at the bottom of the screen in the middle
 	paddle the_paddle(constants::window_width / 2.0f, constants::window_height - constants::paddle_height);
 
+	// Create the grid of bricks
+	// We will use an std::vector to store them
+	std::vector<brick> bricks;
+
+	for (int i = 0; i < constants::brick_columns; ++i) {
+		for (int j = 0; j < constants::brick_rows; ++j) {
+			// Calculate the brick's position
+			float x = constants::brick_offset + (i + 1) * constants::brick_width;
+			float y = (j + 1) * constants::brick_height;
+
+			// Create the brick object
+			bricks.emplace_back(x, y);
+		}
+	}
+
 	// Create the game's window using an object of class RenderWindow
 	// The constructor takes an SFML 2D vector with the window dimensions
 	// and an std::string with the window title
 	// The SFML code is in the sf namespace
-	sf::RenderWindow game_window{ {static_cast<unsigned int>(constants::window_width), static_cast<unsigned int>(constants::window_height)},
-					 "Simple Breakout Game Version 6"s };
+	sf::RenderWindow game_window{ {static_cast<signed int>(constants::window_width), static_cast<signed int>(constants::window_height)},
+		"Simple Breakout Game Version 7"s };
 
 	// Limit the framerate
 	// This allows other processes to run and reduces power consumption
@@ -61,12 +78,20 @@ int main() {
 		the_background.update();
 		the_ball.update();
 		the_paddle.update();
+
+		for (auto& b : bricks)
+			b.update();
+
 		handle_collision(the_ball, the_paddle);
 
 		// Display the updated graphics
 		the_background.draw(game_window);
 		the_ball.draw(game_window);
 		the_paddle.draw(game_window);
+
+		for (auto& b : bricks)
+			b.draw(game_window);
+
 		game_window.display();
 	}
 }
